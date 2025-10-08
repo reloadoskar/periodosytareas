54fdd5577b2ee2b8dc4ba47b462c15596ef34fe6
reloadoskar <reloadoskar@gmail.com>
2025-10-07 18:33:54 -0600

    agregados iconos

--- app/Tareas.jsx
+++ app/Tareas.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import { MdCancel } from "react-icons/md";
+import { FaCheck } from "react-icons/fa";
+import { FaRedo } from "react-icons/fa";
 
 export default function Tareas({ tareas, handleDeleteTarea, handleToggleCompleteTarea, handleMoveTarea, currentPeriodo }) {
     return (
@@ -11,15 +14,15 @@ export default function Tareas({ tareas, handleDeleteTarea, handleToggleComplete
                             <h2 className="titulo">Tareas</h2>
                             <ul className="flex flex-col gap-2">
                                 {tareas.map((tarea, index) => (
-                                    <li className={`flex gap-2 rounded-md ${tarea.animate ? 'animate-bounce' : ''} ${tarea.completed ? "bg-gray-800/30" : ""}`} key={index} >
-                                        <p className={`basis-2/4 font-black text-lg capitalize ${tarea.completed ? "line-through" : ""}`} >
+                                    <li className={`flex gap-2 rounded-md ${tarea.animate ? 'animate-bounce' : ''} ${tarea.completed ? "bg-gray-800/60" : ""}`} key={index} >
+                                        <p className={`basis-4/6 font-black text-lg capitalize ${tarea.completed ? "line-through" : ""}`} >
                                             {tarea.text}
                                         </p>
                                         <button className="basis-1/6" onClick={() => handleToggleCompleteTarea(index)} style={{ marginLeft: "10px" }}>
-                                            {tarea.completed ? "Desmarcar" : "Completar"}
+                                            {tarea.completed ? <FaRedo /> : <FaCheck />}
                                         </button>
                                         <button className="basis-1/6" onClick={() => handleDeleteTarea(index)} style={{ marginLeft: "10px" }}>
-                                            Eliminar
+                                            <MdCancel /> 
                                         </button>
                                     </li>
                                 ))}
