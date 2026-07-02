14de2053999c28bb5b3c32f3596f93571f9358d5
reloadoskar <reloadoskar@gmail.com>
2026-07-01 21:52:17 -0600

    fix en la conexion 4

--- utils/connection.js
+++ utils/connection.js
@@ -27,4 +27,4 @@ export const dbConnect = async (dbName) => {
   return connection;
 };
 
-export const getUsersConnection = () => dbConnect("/PYT_USRS");
+export const getUsersConnection = () => dbConnect("PYT_USRS");
