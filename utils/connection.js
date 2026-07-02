a4dae60afe34f177d1647ba3d7fc494a948b9446
reloadoskar <reloadoskar@gmail.com>
2026-07-01 22:27:50 -0600

    fix en la conexion 7

--- utils/connection.js
+++ utils/connection.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const { MONGO_URL, MONGOUSER, MONGOPASSWORD } = process.env;
+const { MONGO_URL } = process.env;
 
 
 
@@ -8,18 +8,12 @@ const globalCache = globalThis.__mongooseConnections ?? {
   connections: new Map(),
 };
 globalThis.__mongooseConnections = globalCache;
-//agregar user como MONGOUSER y password como MONGOPASSWORD a la url de mongo si es necesario
-const buildMongoUri = (dbName) => {
-  if (MONGOUSER && MONGOPASSWORD) {
-    return `${MONGO_URL}/${dbName}?user=${MONGOUSER}&pass=${MONGOPASSWORD}`;
-  }
-  return `${MONGO_URL}/${dbName}`;
-};
+
+const buildMongoUri = (dbName) => `${MONGO_URL}/${dbName}`;
 
 export const dbConnect = async (dbName) => {
   if (!dbName) throw new Error("dbName is required.");
   if (!MONGO_URL) throw new Error("MONGO_URL is not defined.");
-  if (!MONGOUSER || !MONGOPASSWORD) throw new Error("MONGOUSER and MONGOPASSWORD are not defined.");
 
   if (globalCache.connections.has(dbName)) {
     return globalCache.connections.get(dbName);
